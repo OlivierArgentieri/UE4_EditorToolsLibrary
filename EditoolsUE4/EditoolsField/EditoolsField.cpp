9eb352628a20c9cb7188fd60144ffb0f9e1177be
OlivierArgentieri <olivierargentieri@gmail.com>
2021-01-03 21:46:09 +0100

    update Create TextBox methods

--- EditoolsUE4/EditoolsField/EditoolsField.cpp
+++ EditoolsUE4/EditoolsField/EditoolsField.cpp
@@ -1,11 +1,29 @@
 #include "EditoolsField.h"
 
 
-TSharedRef<SEditableTextBox> EditoolsUE4::EditoolsField::CreateTextBox_Lambda(TFunction<void(const FText&)> _lambdaOnEditedText, TFunction<FText()> _lambdaGetText, FLinearColor _textColor, FLinearColor _backgroundColor)
+TSharedRef<SEditableTextBox> EditoolsUE4::EditoolsField::CreateTextBox(FOnTextChanged _onEditedText, TBaseDelegate<FText> _getText, FLinearColor _textColor, FLinearColor _backgroundColor)
 {
 	return SNew(SEditableTextBox)
 		.BackgroundColor(FSlateColor(_backgroundColor))
-		.OnTextChanged_Lambda(_lambdaOnEditedText)
+		.OnTextChanged(_onEditedText)
+		.Text(_getText.Execute())
+		.ForegroundColor(FSlateColor(_textColor));
+}
+
+TSharedRef<SEditableTextBox> EditoolsUE4::EditoolsField::CreateTextBox_Lambda(TFunction<void(const FText&)> _lambdaOnTextChanged, TFunction<FText()> _lambdaGetText, FLinearColor _textColor, FLinearColor _backgroundColor)
+{
+	return SNew(SEditableTextBox)
+		.BackgroundColor(FSlateColor(_backgroundColor))
+		.OnTextChanged_Lambda(_lambdaOnTextChanged)
 		.Text_Lambda(std::move(_lambdaGetText))
 		.ForegroundColor(FSlateColor(_textColor));
+}
+
+TSharedRef<SEditableTextBox> EditoolsUE4::EditoolsField::CreateTextBox_AllDynamic(TFunction<void(const FText&)> _lambdaOnTextChanged, TFunction<FText()> _lambdaGetText, TFunction<FSlateColor()> _lambdaTextColor, TFunction<FSlateColor()> _lambdaBackgroundColor)
+{
+	return SNew(SEditableTextBox)
+		.BackgroundColor_Lambda(std::move(_lambdaBackgroundColor))
+		.OnTextChanged_Lambda(_lambdaOnTextChanged)
+		.Text_Lambda(std::move(_lambdaGetText))
+		.ForegroundColor_Lambda(std::move(_lambdaTextColor));
 }
--- EditoolsUE4/EditoolsField/EditoolsField.h
+++ EditoolsUE4/EditoolsField/EditoolsField.h
@@ -10,14 +10,35 @@ namespace EditoolsUE4
 	public:
 
 		/// <summary>
-		/// Create text box with lambda event
+		/// CreateTextBox create TextBox with callback. usage : TBaseDelegate<FText>.CreateRaw(this,...)
 		/// </summary>
-		/// <param name="_lambdaOnEditedText"></param>
+		/// <param name="_onEditedText"></param>
+		/// <param name="_getText"></param>
+		/// <param name="_textColor"></param>
+		/// <param name="_backgroundColor"></param>
+		/// <returns></returns>
+		static TSharedRef<SEditableTextBox> CreateTextBox(FOnTextChanged _onEditedText, TBaseDelegate<FText> _getText, FLinearColor _textColor = FLinearColor::Black, FLinearColor _backgroundColor = FLinearColor::White);
+
+		
+		/// <summary>
+		/// Create TextBox with getter and edit event has lambda event
+		/// </summary>
+		/// <param name="_lambdaOnTextChanged"></param>
 		/// <param name="_lambdaGetText"></param>
 		/// <param name="_textColor"></param>
 		/// <param name="_backgroundColor"></param>
 		/// <returns></returns>
-		static TSharedRef<SEditableTextBox> CreateTextBox_Lambda(TFunction<void(const FText&)> _lambdaOnEditedText, TFunction<FText()> _lambdaGetText, FLinearColor _textColor = FLinearColor::Black, FLinearColor _backgroundColor= FLinearColor::White);
+		static TSharedRef<SEditableTextBox> CreateTextBox_Lambda(TFunction<void(const FText&)> _lambdaOnTextChanged, TFunction<FText()> _lambdaGetText, FLinearColor _textColor = FLinearColor::Black, FLinearColor _backgroundColor= FLinearColor::White);
+
+		/// <summary>
+		/// Create TextBox with all params has lambda event
+		/// </summary>
+		/// <param name="_lambdaOnTextChanged"></param>
+		/// <param name="_lambdaGetText"></param>
+		/// <param name="_lambdaTextColor"></param>
+		/// <param name="_lambdaBackgroundColor"></param>
+		/// <returns></returns>
+		static TSharedRef<SEditableTextBox> CreateTextBox_AllDynamic(TFunction<void(const FText&)> _lambdaOnTextChanged, TFunction<FText()> _lambdaGetText, TFunction<FSlateColor()> _lambdaTextColor, TFunction<FSlateColor()> _lambdaBackgroundColor);
 
 	};
 
